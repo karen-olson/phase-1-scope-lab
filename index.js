5ade50f2127845cf4b8f5215dae1500ab5c259b4
Karen Olson <karmarie@gmail.com>
2021-07-26 17:31:52 -0500

    commit tree 5e84297e3780ea4794dd4035ae3c713167d194cf

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    // console.log(customerName);
+    customerName = customerName.toUpperCase();
+    // console.log(customerName);
+    return customerName;
+}
+
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'obviously bob';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'tim';
+}
